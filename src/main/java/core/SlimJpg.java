dc0a3ce53c4fe422ffbfb61993f3d6aebd71dda4
Roc Boronat <roc@fewlaps.com>
2019-06-07 11:45:14 -0500

    Remove the logger and unneeded variables

--- src/main/java/core/SlimJpg.java
+++ src/main/java/core/SlimJpg.java
@@ -1,7 +1,6 @@
 package core;
 
 import utils.ImageUtils;
-import utils.ReadableUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -10,40 +9,18 @@ import java.io.IOException;
 
 public class SlimJpg {
 
-    public static final int MIN_JPEG_QUALITY = 0;
-    public static final int MAX_JPEG_QUALITY = 100;
-    private Logger logger;
+    private static final int MIN_JPEG_QUALITY = 0;
+    private static final int MAX_JPEG_QUALITY = 100;
 
     private byte[] src;
 
     public SlimJpg(byte[] src) {
-        logger = new Logger() {
-            @Override
-            public void log(String txt) {
-            }
-
-            @Override
-            public void warn(String txt) {
-            }
-
-            @Override
-            public void error(String txt) {
-            }
-
-            @Override
-            public void success(String txt) {
-            }
-        };
         this.src = src;
-    }
-
-    public void setLoger(Logger logger) {
-        this.logger = logger;
     }
 
     public Result optimize(double maxVisualDiff, long minFileSizeToOptimize, boolean keepMetadata) throws IOException {
         long start = System.currentTimeMillis();
-        InternalResult optimizedPicture = optimizePicture(maxVisualDiff, keepMetadata);
+        InternalResult optimizedPicture = isThisQualityTooHigh(maxVisualDiff, keepMetadata);
         long end = System.currentTimeMillis();
 
         long elapsedTime = end - start;
@@ -57,29 +34,28 @@ public class SlimJpg {
         );
     }
 
-    private InternalResult optimizePicture(double maxVisualDiff, boolean keepMetadata) throws IOException {
+    private InternalResult isThisQualityTooHigh(double maxVisualDiff, boolean keepMetadata) throws IOException {
         BufferedImage img1 = ImageIO.read(new ByteArrayInputStream(src));
 
         int minQ = MIN_JPEG_QUALITY;
         int maxQ = MAX_JPEG_QUALITY;
-        int foundQuality = -1;
+        int quality = -1;
+
         while (minQ <= maxQ) {
-            logger.log(" - Dichotomic search between (" + minQ + ", " + maxQ + ") qualities :");
-            int quality = (int) Math.floor((minQ + maxQ) / 2.);
-            if (optimizePicture(img1, quality, maxVisualDiff) == true) {
-                foundQuality = quality;
+            quality = (int) Math.floor((minQ + maxQ) / 2.);
+
+            if (isThisQualityTooHigh(img1, quality, maxVisualDiff)) {
                 maxQ = quality - 1;
             } else {
                 minQ = quality + 1;
             }
         }
 
-
-        if (foundQuality < MAX_JPEG_QUALITY) {
-            byte[] optimizedImage = ImageUtils.createJPEG(src, foundQuality, keepMetadata);
+        if (quality < MAX_JPEG_QUALITY) {
+            byte[] optimizedImage = ImageUtils.createJPEG(src, quality, keepMetadata);
             return new InternalResult(
                     optimizedImage,
-                    foundQuality
+                    quality
             );
         } else {
             return new InternalResult(
@@ -89,27 +65,13 @@ public class SlimJpg {
         }
     }
 
-    private boolean optimizePicture(BufferedImage img1, int quality, double maxVisualDiff) throws IOException {
-        logger.log("   Trying quality " + quality + "%");
+    private boolean isThisQualityTooHigh(BufferedImage img1, int quality, double maxVisualDiff) throws IOException {
+        byte[] optimizedPicture = ImageUtils.createJPEG(src, quality, true);
 
-        long start1 = System.currentTimeMillis();
-        byte[] _tmp = ImageUtils.createJPEG(src, quality, true);
-        long end1 = System.currentTimeMillis();
-        logger.log("   * Size : " + ReadableUtils.fileSize(_tmp.length) + "\t (" + ReadableUtils.interval(end1 - start1) + ")");
-
-        long start2 = System.currentTimeMillis();
-        BufferedImage img2 = ImageIO.read(new ByteArrayInputStream(_tmp));
+        BufferedImage img2 = ImageIO.read(new ByteArrayInputStream(optimizedPicture));
         double diff = ImageUtils.computeSimilarityRGB(img1, img2);
-        long end2 = System.currentTimeMillis();
 
-        logger.log("   * Diff : " + ReadableUtils.rate(diff) + "\t (" + ReadableUtils.interval(end2 - start2) + ")");
         diff *= 100.;
-        if (diff < maxVisualDiff) {
-            logger.log("   [OK] Visual diff is correct.");
-            return true;
-        } else {
-            logger.log("   [KO] Visual diff is too important, try a better quality.");
-            return false;
-        }
+        return diff < maxVisualDiff;
     }
 }
