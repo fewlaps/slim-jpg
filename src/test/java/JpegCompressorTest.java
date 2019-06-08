39a0064d6eb51f333d7e368a802a9c9eccc97b2b
Roc Boronat <roc@fewlaps.com>
2019-06-08 16:46:50 -0500

    Generate 101 images to see how JPEG quality impacts the result

---
+++ src/test/java/JpegCompressorTest.java
@@ -0,0 +1,32 @@
+import com.fewlaps.slimjpg.core.util.JpegCompressor;
+import file.BinaryFileWriter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JpegCompressorTest extends BaseTest {
+
+    private static final String OUT_DIRECTORY = "out/jpeg-qualities/";
+
+    @Test
+    public void useAllJpegQualities() throws IOException {
+        JpegCompressor compressor = new JpegCompressor();
+        byte[] image = getBytes(AVATAR);
+
+        File directory = new File(OUT_DIRECTORY);
+        directory.mkdirs();
+        BinaryFileWriter writer = new BinaryFileWriter();
+
+        for (int quality = 0; quality <= 100; quality++) {
+            long start = System.currentTimeMillis();
+
+            byte[] result = compressor.writeJpg(image, quality, false);
+
+            long time = System.currentTimeMillis() - start;
+            System.out.println("Compressing image in quality " + quality + " took " + time + "ms");
+
+            writer.write(result, OUT_DIRECTORY + "quality-" + quality + ".jpg");
+        }
+    }
+}
