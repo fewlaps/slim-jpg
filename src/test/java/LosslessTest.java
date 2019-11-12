2bee39cd4567c4575b7ad3927d980b5f97896578
Roc Boronat <roc@fewlaps.com>
2019-11-12 16:45:53 +0100

    Check that the lossless optimizations don't do any iteration to find that they have to use the 100% quality

--- src/test/java/LosslessTest.java
+++ src/test/java/LosslessTest.java
@@ -4,13 +4,16 @@ public class LosslessTest extends BaseTest {
 
     private static final int IGNORE_MAX_WEIGHT = -1;
 
+    /** These tests also check that the way to know the JPEG quality
+     */
+
     @Test
     public void losslessOptimizations_inOptimizableFiles_shouldReturnANewFile() {
-        test(CHINA, 389341, 0, IGNORE_MAX_WEIGHT, true);
+        test(CHINA, 376871, 0, 0, IGNORE_MAX_WEIGHT, true);
     }
 
     @Test
     public void losslessOptimizations_inUnoptimizableFiles_shouldReturnTheOriginalFile() {
-        test(AVATAR, getWeight(AVATAR), 0, IGNORE_MAX_WEIGHT, true);
+        test(AVATAR, getWeight(AVATAR), 0, 0, IGNORE_MAX_WEIGHT, true);
     }
 }
