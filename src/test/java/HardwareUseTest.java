b7fc537b58ad36bf2847ee2f483e5ca88ddee3e9
Roc Boronat <roc@fewlaps.com>
2019-11-15 17:28:29 +0100

    Add a test that prints the memory usage during a long aggressive use, to check that there's no memory leak

---
+++ src/test/java/HardwareUseTest.java
@@ -0,0 +1,39 @@
+import com.fewlaps.slimjpg.SlimJpg;
+import org.junit.Test;
+
+public class HardwareUseTest extends BaseTest {
+
+    @Test
+    public void printMemoryUsage_inAnAggressiveUsage() {
+        System.out.println("Memory used when the test starts: " + usedMemory() + " MB");
+
+        long startTime = System.currentTimeMillis();
+
+        long i = 0;
+        long testTime = 1000 * 60 * 10;
+        while ((startTime + testTime) > System.currentTimeMillis()) {
+            for (byte[] picture : pictures) {
+                doCommonCall(picture);
+            }
+
+            double usedMemoryInMB = usedMemory();
+            System.out.println("Iteration " + i++ + " using " + usedMemoryInMB + " MB");
+        }
+    }
+
+    private void doCommonCall(byte[] picture) {
+        SlimJpg.file(picture)
+                .maxVisualDiff(0.5)
+                .maxFileWeightInKB(200)
+                .useOptimizedMetadata()
+                .optimize();
+    }
+
+    double usedMemory() {
+        Runtime runtime = Runtime.getRuntime();
+        long totalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        double usedMemory = (double) (totalMemory - freeMemory) / (double) (1024 * 1024);
+        return usedMemory;
+    }
+}
